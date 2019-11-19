216e2ce1c73d6e5b51da099c7209edaed0b34f03
gmn14 <meza.gustavo@pucp.edu.pe>
2019-11-19 12:23:06 -0500

    DG... javascript

---
+++ src/java/Dtos/EstadisticasP.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dtos;
+
+/**
+ *
+ * @author Gustavo_Meza
+ */
+public class EstadisticasP {
+    private int codigo;
+    private String nombre;
+    private String condicion;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the condicion
+     */
+    public String getCondicion() {
+        return condicion;
+    }
+
+    /**
+     * @param condicion the condicion to set
+     */
+    public void setCondicion(String condicion) {
+        this.condicion = condicion;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+}
