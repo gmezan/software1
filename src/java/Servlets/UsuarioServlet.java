6a2a501f242a8e603a6beed2eac427f7f035ddf9
gmn14 <meza.gustavo@pucp.edu.pe>
2019-12-17 00:42:38 -0500

    xd

--- src/java/Servlets/UsuarioServlet.java
+++ src/java/Servlets/UsuarioServlet.java
@@ -68,7 +68,7 @@ public class UsuarioServlet extends HttpServlet {
                 break;
                 
             case "listaNR":
-                //UsuarioDao.sendCorreo("gustavomeza27@gmail.com", "gmn-14899", correos,"HOLA", "hola");
+                //UsuarioDao.sendCorreo("gustavomeza27@gmail.com", "AAAAAAAAA", correos,"HOLA", "hola");
                 request.setAttribute("listaNR", uDao.listarUsuarioNR());
                 view = request.getRequestDispatcher("/DG/peopleNR.jsp");
                 view.forward(request, response);
